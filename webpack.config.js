51a0eb13f70c2edd192ec05b48936b48e5b1eb57
Simon O'Hara <simonpohara@gmail.com>
2022-03-12 11:19:57 +1100

    Webpack config to generate index.html properly. Set up webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
 const path = require('path');
+// eslint-disable-next-line node/no-unpublished-require
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// eslint-disable-next-line node/no-unpublished-require
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
   devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 8080,
+    watchFiles: ['src/**/*', 'assets/**/*'],
+  },
   mode: 'development',
   module: {
     rules: [
@@ -18,6 +30,13 @@ module.exports = {
   },
   output: {
     filename: 'path-generator.bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Path Generator',
+      template: './assets/index.html',
+    }),
+  ],
 };
